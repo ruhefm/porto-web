e099ccc7b8781992f319e6e7d69b0647eba0d388
ruhefm <64914663+ruhefm@users.noreply.github.com>
2025-06-06 14:46:42 +0700

    changing from sm to md ed section

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="">
   <ul class="list-inside text-left" v-for="(education, index) in educations" :key="index">
-    <li class="block py-2 sm:flex border-b items-center">
+    <li class="block py-2 md:flex border-b items-center">
       <div class="items-center justify-center flex py-4"><img :src="`/${education.path}`" :alt="education.title" class="w-16 h-16 rounded-full object-cover bg-white"></div>
       <div>
-      <p class="px-4 text-xl sm:text-2xl text-center sm:text-left">{{ education.title }}</p>
-      <p class="px-4 text-lg sm:text-xl text-center sm:text-left">{{ education.dateRange }}</p>
-      <p class="px-4 text-lg sm:text-xl text-center sm:text-left">{{ education.major }}</p>
+      <p class="px-4 text-xl md:text-2xl text-center md:text-left">{{ education.title }}</p>
+      <p class="px-4 text-lg md:text-xl text-center md:text-left">{{ education.dateRange }}</p>
+      <p class="px-4 text-lg md:text-xl text-center md:text-left">{{ education.major }}</p>
       <li class="px-4 list-disc" v-for="(line, idx) in education.desc.split('\n')" :key="idx">
     {{ line.replace(/^•\s*/, '') }}
   </li>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,23 @@
+<template>
+	<section class="">
+	  <ul class="list-inside text-left" v-for="(education, index) in educations" :key="index">
+	    <li class="block py-2 sm:flex border-b items-center">
+	      <div class="items-center justify-center flex py-4"><img :src="`/${education.path}`" :alt="education.title" class="w-16 h-16 rounded-full object-cover bg-white"></div>
+	      <div>
+	      <p class="px-4 text-xl sm:text-2xl text-center sm:text-left">{{ education.title }}</p>
+	      <p class="px-4 text-lg sm:text-xl text-center sm:text-left">{{ education.dateRange }}</p>
+	      <p class="px-4 text-lg sm:text-xl text-center sm:text-left">{{ education.major }}</p>
+	      <li class="px-4 list-disc" v-for="(line, idx) in education.desc.split('\n')" :key="idx">
+	    {{ line.replace(/^•\s*/, '') }}
+	  </li>
+	      </div>
+	    </li>
+	  </ul>
+	  </section>
+</template>
+
+<script setup lang="ts">
+defineProps<{ 
+	educations: { title: string; dateRange: string; path: string; major: string; desc: string; }[];
+}>()
+</script>
