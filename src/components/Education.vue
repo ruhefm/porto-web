c432bcc4afdd8d4e48eae467e63d8f5dae0e8fb1
ruhefm <64914663+ruhefm@users.noreply.github.com>
2025-06-06 14:36:32 +0700

    update responsive

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -4,9 +4,9 @@
     <li class="block py-2 sm:flex border-b items-center">
       <div class="items-center justify-center flex py-4"><img :src="`/${education.path}`" :alt="education.title" class="w-16 h-16 rounded-full object-cover bg-white"></div>
       <div>
-      <span class="px-4 text-xl sm:text-2xl">{{ education.title }}</span><br/>
-      <span class="px-4 text-lg sm:text-xl">{{ education.dateRange }}</span><br/>
-      <span class="px-4 text-lg sm:text-xl">{{ education.major }}</span><br/>
+      <p class="px-4 text-xl sm:text-2xl text-center sm:text-left">{{ education.title }}</p>
+      <p class="px-4 text-lg sm:text-xl text-center sm:text-left">{{ education.dateRange }}</p>
+      <p class="px-4 text-lg sm:text-xl text-center sm:text-left">{{ education.major }}</p>
       <li class="px-4 list-disc" v-for="(line, idx) in education.desc.split('\n')" :key="idx">
     {{ line.replace(/^•\s*/, '') }}
   </li>
