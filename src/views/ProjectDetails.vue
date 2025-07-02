26d5b51a27371cb39f3f85d63e43b2b5099a0066
ruhefm <64914663+ruhefm@users.noreply.github.com>
2025-07-02 13:41:04 +0700

    test rename

--- src/views/ProjectDetails.vue
+++ src/views/ProjectDetails.vue
@@ -2,12 +2,12 @@
 
   <mainMenu />
     <main class="mx-auto max-w-screen-lg p-8">
-      <projectDetail />
+      <ProjectDetail />
     </main>
 
 </template>
 
 <script setup lang="ts">
-import projectDetail from '@/components/Projects/ProjectDetail.vue'
+import ProjectDetail from '@/components/Projects/ProjectDetail.vue'
 import mainMenu from '@/components/MainMenu.vue'
 </script>
