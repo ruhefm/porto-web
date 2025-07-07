262f82c88e6d13a9acd12811e4aa857636ffccdf
ruhefm <64914663+ruhefm@users.noreply.github.com>
2025-07-07 15:06:48 +0700

    add robots.txt, update css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <main class="text-gray-300 font-noto font-light text-sm sm:text-lg mx-auto">
     <router-view />
-      </main>
     <Footer />
+      </main>
 </template>
 
 <script setup lang="ts">
